e467938f024636ac641bc4dbcd676d0cb92f8b17
Notebook Asusi7 Prisma <development.luca@gmail.com>
2018-08-29 18:31:55 +0200

    deploy heroku app

--- server.js
+++ server.js
@@ -2,17 +2,17 @@ const express = require('express');
 
 const app = express();
 
+app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
-app.post('/hello' , (req, res) => {
+app.get('/hello' , (req, res) => {
     console.log('GET for /hello');
-    
-   // res.write('sei su homes');
+    // res.write('sei su homes');
 })
 
-app.listen(process.env.port || 8080 , () => {
-    console.log('server started!')
+app.listen(app.get('port'), () => {
+    console.log('Node running on port ' , app.get('port'));
 });
 
 
