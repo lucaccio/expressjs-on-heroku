e523a35a3c1f714114ad24996ee97fe89c998ca0
Notebook Asusi7 Prisma <development.luca@gmail.com>
2018-08-30 10:12:43 +0200

    prova mysql

--- server.js
+++ server.js
@@ -1,14 +1,51 @@
+
+const debug = require('debug')('my-debug');
+ 
 const express = require('express');
 
 const app = express();
+
+const mysql = require('mysql')
+
+const conn = mysql.createConnection({
+    host : 'localhost',
+    database : 'sandbox',
+    user : 'root',
+    password:''
+});
+
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/hello' , (req, res) => {
-    console.log('GET for /hello');
-    // res.write('sei su homes');
+app.get('/testdb' , (req, res) => {
+    console.log('GET for /testdb');
+    
+    conn.connect(function(err){
+        if(err) {
+            console.log(err.code); // 'ECONNREFUSED'
+            console.log(err.fatal); // true
+            console.log(err.message);
+        } else {
+            conn.query('SELECT * FROM users', function (error, results, fields) {
+                if (error) throw error;
+                  console.log('User: ', results[0].username);
+              });
+              conn.end();
+        }
+        
+    });
+   
+    
+    
+    
+
+
+
+
+
+ 
 })
 
 app.listen(app.get('port'), () => {
