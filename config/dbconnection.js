52504dbb6f376cd34dfa66e4aa3a422a97d00f60
Notebook Asusi7 Prisma <development.luca@gmail.com>
2018-08-31 10:39:57 +0200

    modifica variabili ambiente mysql

--- config/dbconnection.js
+++ config/dbconnection.js
@@ -6,10 +6,10 @@ var connection = connect();
  function connect() { 
 
     var connection  = mysql.createConnection({
-        host : 'localhost',
-        database : 'sandbox',
-        user : 'root',
-        password:'prisma2013'
+        host : process.env.DB_HOST,
+        database : process.env.DB_NAME,
+        user : process.env.DB_USER,
+        password:process.env.DB_PASS,
     });
      
     connection.connect();
